d31c126f7bb4ddfa9e75680c0a9fef10d2a8420d
digital-nishtyak <lapaev.yarik@mail.ru>
2024-02-12 16:57:50 +0300

    hw1 done

--- bot/src/main/java/edu/java/bot/controller/NotificationUpdateListener.java
+++ bot/src/main/java/edu/java/bot/controller/NotificationUpdateListener.java
@@ -9,11 +9,11 @@ import jakarta.annotation.PostConstruct;
 import java.util.List;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
 
-@Service
 @Log4j2
 @Getter
+@Controller
 public class NotificationUpdateListener implements UpdatesListener {
 
     private final TelegramBot telegramBot;
