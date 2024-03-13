5a21d5b95fbb66d002e69675c09478f4a96cfc94
digital-nishtyak <lapaev.yarik@mail.ru>
2024-03-13 19:15:01 +0300

    MIGRATIONS FIX

--- migrations/02-create_links.sql
+++ migrations/02-create_links.sql
@@ -1,6 +1,6 @@
 create table links
 (
-    link_id         bigint generated always as identity,
-    created_at      timestamp with time zone not null,
-    primary key (link_id)
+    link       text,
+    created_at timestamp with time zone not null,
+    primary key (link)
 );
--- migrations/03-create_relationsChatLinks.sql
+++ migrations/03-create_relationsChatLinks.sql
@@ -1,8 +1,8 @@
 create table chat_link_relation
 (
     chat_id bigint not null,
-    link_id bigint not null,
-    primary key (chat_id, link_id),
+    link text not null,
+    primary key (chat_id, link),
     foreign key (chat_id) references chats (telegram_chat_id),
-    foreign key (link_id) references links (link_id)
+    foreign key (link) references links (link)
 );
