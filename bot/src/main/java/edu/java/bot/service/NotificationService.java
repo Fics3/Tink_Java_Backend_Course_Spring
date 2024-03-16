0a7c3997bf9d3ab474ac840a18d9fa8f858cb408
digital-nishtyak <lapaev.yarik@mail.ru>
2024-03-17 00:36:47 +0300

    hw3

--- bot/src/main/java/edu/java/bot/service/NotificationService.java
+++ bot/src/main/java/edu/java/bot/service/NotificationService.java
@@ -47,7 +47,4 @@ public class NotificationService {
         telegramBot.execute(message);
     }
 
-    public boolean messageIsNull(Update update) {
-        return update.message() == null;
-    }
 }
