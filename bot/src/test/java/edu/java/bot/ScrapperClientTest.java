b53c967fcd65f7f7528491d9d2f914d8270719b7
digital-nishtyak <lapaev.yarik@mail.ru>
2024-03-20 02:27:10 +0300

    lower case bug fix code cleanup

--- bot/src/test/java/edu/java/bot/ScrapperClientTest.java
+++ bot/src/test/java/edu/java/bot/ScrapperClientTest.java
@@ -1,143 +1,52 @@
 package edu.java.bot;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import edu.java.bot.client.ScrapperClient;
-import java.net.URI;
-import java.time.OffsetDateTime;
-import java.util.Collections;
-import lombok.extern.log4j.Log4j2;
-import org.example.dto.AddLinkRequest;
-import org.example.dto.LinkResponse;
-import org.example.dto.ListLinkResponse;
-import org.example.dto.RemoveLinkRequest;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
-import reactor.test.StepVerifier;
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.delete;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-@Log4j2
+@WebFluxTest(ScrapperClient.class)
 class ScrapperClientTest {
 
-    private static WireMockServer wireMockServer;
+    @Autowired
+    private WebTestClient webTestClient;
 
-    @BeforeAll
-    static void setUp() {
-        wireMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort());
-        wireMockServer.start();
-        WireMock.configureFor("localhost", wireMockServer.port());
-    }
+    @MockBean
+    private ScrapperClient scrapperClient;
 
-    @AfterAll
-    static void tearDown() {
-        wireMockServer.stop();
+    @Test
+    @DisplayName("Should return code 404 when register chat")
+    void registerChat_shouldReturnInternalServerError() {
+        // Arrange
+        long chatId = 123;
+        Mockito.when(scrapperClient.registerChat(chatId))
+            .thenReturn(Mono.error(new RuntimeException("Some error message")));
+
+        // Act & Assert
+        webTestClient.post()
+            .uri("/tg-chat/{id}", chatId)
+            .exchange()
+            .expectStatus().is4xxClientError();
     }
 
     @Test
-    @DisplayName("Register Chat - Successfully Registered")
-    void registerChatTest() {
-        long chatId = 123456789L;
-        ScrapperClient scrapperClient = mock(ScrapperClient.class);
-        Mockito.when(scrapperClient.getChat()).thenReturn("/tg-chat/");
+    @DisplayName("Should return code 404 when delete chat")
+    void deleteChat_shouldReturnInternalServerError() {
+        // Arrange
+        long chatId = 123;
+        Mockito.when(scrapperClient.deleteChat(chatId))
+            .thenReturn(Mono.error(new RuntimeException("Some error message")));
 
-        when(scrapperClient.registerChat(anyLong())).thenReturn(Mono.just("Registration successful"));
-
-        stubFor(post(urlEqualTo(scrapperClient.getChat() + chatId))
-            .willReturn(aResponse()
-                .withStatus(200)
-                .withBody("Registration successful")));
-
-        StepVerifier.create(scrapperClient.registerChat(chatId))
-            .expectNext("Registration successful")
-            .expectComplete()
-            .verify();
+        // Act & Assert
+        webTestClient.delete()
+            .uri("/tg-chat/{id}", chatId)
+            .exchange()
+            .expectStatus().is4xxClientError();
     }
 
-    @Test
-    @DisplayName("Delete Chat - Successfully Deleted")
-    void deleteChatTest() {
-        long chatId = 123456789L;
-        ScrapperClient scrapperClient = mock(ScrapperClient.class);
-        Mockito.when(scrapperClient.getChat()).thenReturn("/tg-chat/");
-
-        when(scrapperClient.deleteChat(anyLong())).thenReturn(Mono.just("Delete successful"));
-
-        stubFor(delete(urlEqualTo(scrapperClient.getChat() + chatId))
-            .willReturn(aResponse()
-                .withStatus(200)
-                .withBody("Delete successful")));
-
-        StepVerifier.create(scrapperClient.deleteChat(chatId))
-            .expectNext("Delete successful")
-            .expectComplete()
-            .verify();
-    }
-
-    @Test
-    @DisplayName("Get All Links - Successfully Retrieved")
-    void getAllLinks_SuccessfullyRetrieved() {
-        ScrapperClient scrapperClient = mock(ScrapperClient.class);
-        Mockito.when(scrapperClient.getLinks()).thenReturn("/links/");
-
-        ListLinkResponse expectedResponse = new ListLinkResponse(Collections.emptyList(), 0);
-        when(scrapperClient.getAllLinks(123456789L)).thenReturn(Mono.just(expectedResponse));
-
-        StepVerifier.create(scrapperClient.getAllLinks(123456789L))
-            .expectNext(expectedResponse)
-            .expectComplete()
-            .verify();
-    }
-
-    @Test
-    @DisplayName("Add Link - Successfully Added")
-    void addLink_SuccessfullyAdded() {
-        ScrapperClient scrapperClient = mock(ScrapperClient.class);
-        Mockito.when(scrapperClient.getLinks()).thenReturn("/links/");
-
-        AddLinkRequest addLinkRequest = new AddLinkRequest(URI.create("example.com"));
-        LinkResponse expectedResponse = new LinkResponse(
-            URI.create("example.com"),
-            OffsetDateTime.now()
-        );
-        when(scrapperClient.addLink(eq(123456789L), any(AddLinkRequest.class))).thenReturn(Mono.just(expectedResponse));
-
-        StepVerifier.create(scrapperClient.addLink(123456789L, addLinkRequest))
-            .expectNext(expectedResponse)
-            .expectComplete()
-            .verify();
-    }
-
-    @Test
-    @DisplayName("Remove Link - Successfully Removed")
-    void removeLink_SuccessfullyRemoved() {
-        ScrapperClient scrapperClient = mock(ScrapperClient.class);
-        Mockito.when(scrapperClient.getLinks()).thenReturn("/links/");
-
-        RemoveLinkRequest removeLinkRequest = new RemoveLinkRequest(URI.create("example.com"));
-        LinkResponse expectedResponse = new LinkResponse(
-            URI.create("example.com"),
-            OffsetDateTime.now()
-        );
-        when(scrapperClient.removeLink(eq(123456789L), any(RemoveLinkRequest.class))).thenReturn(Mono.just(
-            expectedResponse));
-
-        StepVerifier.create(scrapperClient.removeLink(123456789L, removeLinkRequest))
-            .expectNext(expectedResponse)
-            .expectComplete()
-            .verify();
-    }
 }
