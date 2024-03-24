86fc9bebc5fa8b933b4f192c5d20af32428eb102
digital_nishtyak <114524392+Fics3@users.noreply.github.com>
2024-03-24 23:40:48 +0300

    Update LinkUpdaterScheduler.java for checkstyle

--- scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Log4j2
 @Component
 public class LinkUpdaterScheduler {
-  
+
     @Scheduled(fixedDelayString = "#{@scheduler.interval()}")
     public void update() {
         log.info("Произошло обновление");
