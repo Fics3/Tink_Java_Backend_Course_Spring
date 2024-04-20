3c393254021e749a0cb2b5a3c6fca33807d67c0a
digital-nishtyak <lapaev.yarik@mail.ru>
2024-04-21 02:57:02 +0300

    hw6 minor fix

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -1,14 +1,15 @@
 package edu.java;
 
-import edu.java.configuration.ApplicationConfig;
-import edu.java.configuration.ClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties({ApplicationConfig.class, ClientConfig.class})
+@EnableConfigurationProperties({
+    edu.java.configuration.ApplicationConfig.class,
+    edu.java.configuration.ClientConfig.class
+})
 @EnableScheduling
 public class ScrapperApplication {
     public static void main(String[] args) {
