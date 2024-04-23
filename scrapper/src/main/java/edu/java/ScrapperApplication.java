4dfb62095cf3afd4381adfe2fa4f6b16d48d3162
digital-nishtyak <lapaev.yarik@mail.ru>
2024-04-23 14:06:09 +0300

    hw6 config properties fix

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -2,14 +2,11 @@ package edu.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-    edu.java.configuration.ApplicationConfig.class,
-    edu.java.configuration.ClientConfig.class
-})
+@ConfigurationPropertiesScan("edu.java.configuration")
 @EnableScheduling
 public class ScrapperApplication {
     public static void main(String[] args) {
